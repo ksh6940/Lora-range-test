67e80d960ecf1e2dbf3a31f70916e8a4dd30926f
Seogo <77395226+ksh6940@users.noreply.github.com>
2025-04-04 10:16:21 +0900

    Update Transmitter.ino

--- Transmitter.ino
+++ Transmitter.ino
@@ -1,12 +1,30 @@
-#define LED_pin 13
+#include <SPI.h>
+#include <SD.h>
+
+#define LED_pin 7
+
 const int btn_pin = 2;
 int buttonState = 0;
 int lastButtonState = 0;
 
+File myFile;
+
 void setup() {
   Serial.begin(57600);
+  Serial.print("Initializing SD Card...");
+
+  if (!SD.begen(4)){
+    Serial.println("Initialization failed!");
+    while(1);
+  }
+  Serial.println("Initialization done.");
+
+  Serial.print("Intializing Module...");
+
   pinMode(LED_pin, OUTPUT);
   pinMode(btn_pin, INPUT);
+
+  Serial.print("Intializing Module Done")
 }
 
 void loop() {
@@ -23,4 +41,17 @@ void loop() {
   }
 
   lastButtonState = buttonState;
+}
+
+void saveFile(RSSI){
+  myFile = SD.open("test.csv", FILE_WRITE);
+
+  if (myFile) {
+    Serial.print("Writing to test.csv");
+    myFile.println(RSSI);
+    myFile.close();
+    Serial.println("done.")
+  } else {
+    Serial.println("error opening csv file.")
+  }
 }
