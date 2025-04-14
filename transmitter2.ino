b7b82e9a4cf9d3f97959ec966436ccb1787063f1
Seogo <77395226+ksh6940@users.noreply.github.com>
2025-04-14 10:37:18 +0900

    Update transmitter2.ino

--- transmitter2.ino
+++ transmitter2.ino
@@ -1,9 +1,8 @@
 #include <math.h>
-
 float angle = 0.0;
 
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(57600); 
 }
 
 void loop() {
@@ -15,14 +14,9 @@ void loop() {
     angle = 0.0;  
   }
 
-  // 사인 값 출력
-  Serial.print("sin : ");
-  Serial.println(sineValue);
-  delay(100);  // 0.1초 대기
-
   // sineValue가 1.00 또는 -1.00에 가까운 값일 때만 조건을 만족
   if (abs(sineValue - 1.0) < 0.01 || abs(sineValue + 1.0) < 0.01) {
-    Serial.println("AT+SEND=2,1,A");
+    Serial.println("AT+SEND=1,11,Hello World!");
     delay(1000);  // 1초 대기 (송신 간격)
   }
 }
